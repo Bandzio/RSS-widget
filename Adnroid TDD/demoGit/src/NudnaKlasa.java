2e58469065ff0776b79e1d71ba71c7d186e4bb13
Marcin B <mbinne@gmail.com>
2011-02-02 00:54:32 +0100

    Update method to do something.

--- Adnroid TDD/demoGit/src/NudnaKlasa.java
+++ Adnroid TDD/demoGit/src/NudnaKlasa.java
@@ -2,11 +2,14 @@
 public class NudnaKlasa {
 
 	/**
-	 * @param args
+	 * Glówna metoda, cos tam se robi
+	 * @param args - args z inputa
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		System.out.print(true);
 	}
 
 }
