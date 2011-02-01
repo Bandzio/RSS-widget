02fe0edf34cb6e62c5de18c9976ba8a89099c62b
Marcin B <mbinne@gmail.com>
2011-02-01 19:57:43 +0100

    Add come output lines
    Signed-off-by: Marcin B <mbinne@gmail.com>

--- Adnroid TDD/HelloWorld/src/HelloJava.java
+++ Adnroid TDD/HelloWorld/src/HelloJava.java
@@ -7,6 +7,8 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hello world git");
+		System.out.println("I am excited to use you");
 	}
 
 }
